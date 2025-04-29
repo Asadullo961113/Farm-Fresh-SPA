b05fab2525caa4b157d96be730990fe0833b3895
Asadullo961113 <asadillok9@gmail.com>
2025-04-30 08:03:32 +0900

    fix: update deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -10,15 +10,16 @@ npm i yarn -g
 yarn global add serve
 yarn
 yarn run build
+pm2 start "yarn run start:prod" --name=Farm-Fresh-SPA
 
 
 # agar mavjud bo‘lsa — restart, mavjud bo‘lmasa — start
-pm2 describe Farm-Fresh-SPA > /dev/null
-if [ $? -eq 0 ]; then
-  pm2 restart Farm-Fresh-SPA
-else
-  pm2 start "yarn run start:prod" --name=Farm-Fresh-SPA
-fi
+# pm2 describe Farm-Fresh-SPA > /dev/null
+# if [ $? -eq 0 ]; then
+#   pm2 restart Farm-Fresh-SPA
+# else
+#   pm2 start "yarn run start:prod" --name=Farm-Fresh-SPA
+# fi
 
 
 # DEVELOPMENT
